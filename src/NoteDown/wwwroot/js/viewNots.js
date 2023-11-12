19b375ad4e41a1e884f53f8294db1dc49e523ef3
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-11-12 20:12:23 -0300

    adiciona função de visualizar a nota antes de adicionar

---
+++ src/NoteDown/wwwroot/js/viewNots.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('#editar-tab').on('click', function () {
+        $('#visualizar-tab').removeClass('active');
+        $('#editar-tab').addClass('active');
+        $('#visualizar').removeClass('show active');
+        $('#editar').addClass('show active');
+    });
+
+    $('#visualizar-tab').on('click', function () {
+        $('#editar-tab').removeClass('active');
+        $('#visualizar-tab').addClass('active');
+        $('#editar').removeClass('show active');
+        $('#visualizar').addClass('show active');
+
+        // Aqui você pode adicionar lógica para renderizar o markdown em #visualizacao-markdown
+        var markdownText = $('#nota').val(); // Obtém o texto do textarea
+        var markdownHtml = marked(markdownText); // Usa o marked.js para renderizar o markdown
+        $('#visualizacao-markdown').html(markdownHtml); // Insere o HTML renderizado na div de visualização
+    });
+});
