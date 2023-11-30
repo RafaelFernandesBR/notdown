50d2b68f4d813de62737ae2a332578c0d13a915d
Rafael Fernandes <marcorafaeldemelo16@gmail.com>
2023-11-29 23:09:33 -0300

    adiciona cache na nota

--- src/NoteDown/Controllers/HomeController.cs
+++ src/NoteDown/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 using NoteDown.Data.IModels;
 using NoteDown.Data.Models;
 using NoteDown.Models;
@@ -10,11 +12,13 @@ namespace NoteDown.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IDataModel _dataModel;
+        private readonly IMemoryCache _memoryCache;
 
-        public HomeController(ILogger<HomeController> logger, IDataModel dataModel)
+        public HomeController(ILogger<HomeController> logger, IDataModel dataModel, IMemoryCache memoryCache)
         {
             _logger = logger;
             _dataModel = dataModel;
+            _memoryCache = memoryCache;
         }
 
         public IActionResult Index()
@@ -25,6 +29,14 @@ namespace NoteDown.Controllers
         [Route("nots/{id}")]
         public IActionResult GeteNots(string id)
         {
+            // Verificar se os dados estão em cache
+            if (_memoryCache.TryGetValue(id, out var cachedData))
+            {
+                // Se estiverem em cache, retornar os dados do cache diretamente
+                ViewBag.Nota = cachedData;
+                return View();
+            }
+
             var nota = _dataModel.GetOne(id);
 
             if (nota == null)
@@ -33,6 +45,12 @@ namespace NoteDown.Controllers
 
                 return NotFound();
             }
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1)
+            };
+            _memoryCache.Set(id, nota, cacheEntryOptions);
 
             ViewBag.Nota = nota;
             return View();
@@ -49,6 +67,7 @@ namespace NoteDown.Controllers
                 notInclud.Conteudo = nota;
 
                 var updatedNota = _dataModel.UpdateNots(notInclud);
+                _memoryCache.Remove(id);
 
                 return Json(updatedNota);
             }
